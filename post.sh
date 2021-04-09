e6efe6a6970859af3e14cb10d7e779190013a222
Wagner Brahm <wagner@brahm.com.br>
2021-04-09 12:09:14 -0300

    Update post.sh

--- post.sh
+++ post.sh
@@ -76,7 +76,7 @@ sleep 2
 
 echo "Installing the boot manager..."
 pacman -S --noconfirm grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 echo "...done!"
 sleep 2
