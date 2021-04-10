c9d4498bb4d4c003075a2ee9e61f4635b2e0d581
Wagner Brahm <wagner@brahm.com.br>
2021-04-10 11:31:47 -0300

    Update 1-base.sh

--- 1-base.sh
+++ 1-base.sh
@@ -20,26 +20,40 @@ sleep 2
 
 echo ""
 echo ""
-echo ""
 
 echo "Let's get started!"
 sleep 2
+
+echo ""
+echo ""
 
 echo "Wiping the partition table..."
 wipefs --all --force /dev/sda
 echo "...done!"
 sleep 2
 
+echo ""
+echo "*********************************************************************"
+echo ""
+
 echo "Creating a GPT partition table and the /boot (EFI) and / (root) partitions..."
 echo -e ',512M,U\n,,L' | sfdisk --label=gpt /dev/sda
 echo "...done!"
 sleep 2
+
+echo ""
+echo "*********************************************************************"
+echo ""
 
 echo "Formatting the drivers..."
 mkfs.vfat -F32 /dev/sda1
 mkfs.xfs -f /dev/sda2
 echo "...done!"
 sleep 2
+
+echo ""
+echo "*********************************************************************"
+echo ""
 
 echo "Mounting the drives..."
 mount /dev/sda2 /mnt
@@ -49,6 +63,10 @@ lsblk
 echo "...done!"
 sleep 2
 
+echo ""
+echo "*********************************************************************"
+echo ""
+
 echo "Testing the internet connectivity..."
 case "$(curl -s --max-time 2 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
   [23]) echo "HTTP connectivity is up, moving on...";;
@@ -56,6 +74,10 @@ case "$(curl -s --max-time 2 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/
   *) echo "The network is down or very slow";;
 esac
 sleep 2
+
+echo ""
+echo "*********************************************************************"
+echo ""
 
 echo "Finding the best mirror list for downloading Arch Linux..."
 reflector --verbose --latest 100 --protocol https --threads 24 --sort rate --save /etc/pacman.d/mirrorlist
@@ -66,6 +88,10 @@ pacman -Syy
 echo "...done!"
 sleep 2
 
+echo ""
+echo "*********************************************************************"
+echo ""
+
 echo "Updating the keyring..."
 pacman-key --init
 pacman-key --populate archlinux
@@ -74,20 +100,36 @@ pacman -Syy
 echo "...done!"
 sleep 2
 
+echo ""
+echo "*********************************************************************"
+echo ""
+
 echo "Installing the base packages..."
-pacstrap /mnt base linux linux-firmware linux-headers intel-ucode acpi acpi_call lm_sensors dosfstools xfsprogs sudo zsh nano git
+pacstrap /mnt base base-devel linux linux-firmware linux-headers intel-ucode acpi acpi_call dosfstools xfsprogs sudo zsh nano git
 echo "...done!"
 sleep 2
+
+echo ""
+echo "*********************************************************************"
+echo ""
 
 echo "Generating fstab for the drives..."
 genfstab -U -p /mnt >> /mnt/etc/fstab
 echo "...done!"
 sleep 2
 
+echo ""
+echo "*********************************************************************"
+echo ""
+
 echo "Creating a RAM drive..."
 echo "tmpfs    /tmp    tmpfs    rw,nodev,nosuid,size=2G    0 0" >> /mnt/etc/fstab
 echo "...done!"
 sleep 2
+
+echo ""
+echo "*********************************************************************"
+echo ""
 
 echo "Creating the swap file..."
 dd if=/dev/zero of=/mnt/swapfile bs=1M count=34816 status=progress
@@ -96,20 +138,19 @@ echo "/swapfile    none    swap    defaults    0 0" >> /mnt/etc/fstab
 echo "...done!"
 sleep 2
 
+clear
 echo ""
-echo ""
-echo "*********************************************************************"
 echo ""
 echo "Base install has finished!"
 echo ""
 echo ""
-echo "After entering into the new system, get the post.sh script here:"
-echo "https://raw.githubusercontent.com/brahm/another-arch-installer/main/post.sh"
+echo "After entering into the new system, get the 2-config.sh script here:"
+echo "https://raw.githubusercontent.com/brahm/another-arch-installer/main/2-config.sh"
 echo "to continue configuring this system."
 echo ""
 echo ""
 sleep 3
 
 echo "Entering as root into the new system..."
-echo "This will continue from threre..."
+echo "The installation will continue from threre..."
 arch-chroot /mnt
